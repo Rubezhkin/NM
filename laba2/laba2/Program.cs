04182f23000797106907fe1cb629778060d733e4
Pavel Nedobezhkin <96119105+Rubezhkin@users.noreply.github.com>
2023-11-14 12:10:58 +0300

    Update Program.cs

--- laba2/laba2/Program.cs
+++ laba2/laba2/Program.cs
@@ -4,11 +4,11 @@ using System.Transactions;
 
 class Laba2
 {
-    public static void generation(int N, int L, int diap)
+    public static void generation(int N, int L, int diap) // генерация
     {
         decimal[][] matrix = new decimal[N][];
         Random rnd = new Random();
-        for(int i = 0; i < N; i++)
+        for(int i = 0; i < N; i++) // генерация
         {
             matrix[i]= new decimal[Math.Min(L,N-i)];
             matrix[i][0] = rnd.Next(diap);
@@ -18,7 +18,7 @@ class Laba2
                     matrix[i][j] = (decimal)(rnd.Next(2*diap*1000)/1000.0-diap);
         }
 
-        for(int i = N-1; i >=0; i--)
+        for(int i = N-1; i >=0; i--) //перемножение
             for(int j = Math.Min(L-1,N-1-i); j >= 0; j--)
                 for(int ki = 0, kj = j, k = i; k >= 0 && kj < L; k--, kj++, ki++)
                 {
@@ -28,11 +28,11 @@ class Laba2
                         matrix[i][j] = matrix[k][ki]*matrix[k][kj];
                 }
 
-        decimal[] x = new decimal[N];
+        decimal[] x = new decimal[N]; // генерация решения
         for(int i = 0; i < N; i++)
             x[i] = (decimal)(rnd.Next(diap*2*1000)/1000.0-diap);
 
-        decimal[] f = new decimal[N];
+        decimal[] f = new decimal[N]; // составление f
         for(int i = 0; i < N; i++)
         {
             f[i] = 0;
@@ -41,7 +41,7 @@ class Laba2
             for(int ki = 1, k = i-1; ki < L && k >= 0; ki++,k--)
                 f[i]+=x[k]*matrix[k][ki];
         }
-        using(StreamWriter sw = new StreamWriter("data.txt"))
+        using(StreamWriter sw = new StreamWriter("data.txt")) // можешь тут путь указать тут запись
         {
             sw.Write($"{N} {L}\n");
             for(int i = 0; i < N; i++)
@@ -68,17 +68,17 @@ class Laba2
             sw.Close();
         }
     }
-    public static bool solution()
+    public static bool solution() // решение
     {
-        using (StreamReader sr = new StreamReader("data.txt"))
+        using (StreamReader sr = new StreamReader("data.txt")) // можешь тут путь указать
         {
-            string nums = sr.ReadLine();
+            string nums = sr.ReadLine(); // чтение с файла
             string[] nums_arr = nums.Split(' ');
             int N, L;
             N = int.Parse(nums_arr[0]);
             L = int.Parse(nums_arr[1]);
 
-            decimal[][] matrix = new decimal[N][];
+            decimal[][] matrix = new decimal[N][];  // матрица
             for (int i = 0; i < N; i++)
             {
                 matrix[i] = new decimal[Math.Min(L, N - i)];
@@ -88,13 +88,13 @@ class Laba2
                     matrix[i][j] = decimal.Parse(nums_arr[j]);
             }
 
-            decimal[] f = new decimal[N];
+            decimal[] f = new decimal[N]; // f 
             nums = sr.ReadLine();
             nums_arr = nums.Split(' ');
             for (int i = 0; i < N; i++)
                 f[i] = decimal.Parse(nums_arr[i]);
 
-            decimal[] x_t = new decimal[N];
+            decimal[] x_t = new decimal[N]; // иксы для оценки погрешности
             nums = sr.ReadLine();
             nums_arr = nums.Split(' ');
             for (int i = 0; i < N; i++)
@@ -103,7 +103,7 @@ class Laba2
             bool flag = true;
             for (int i = 0; i < N && flag; i++)
             {
-                for (int ki = i - 1, kj = 1; ki >= 0 && kj < L; ki--, kj++)
+                for (int ki = i - 1, kj = 1; ki >= 0 && kj < L; ki--, kj++) // преобразование матрицы методом квадратного корня
                     matrix[i][0] -= matrix[ki][kj] * matrix[ki][kj];
                 if (matrix[i][0] < 0)
                     flag = false;
@@ -122,20 +122,20 @@ class Laba2
             {
                 for (int i = 0; i < N; i++)
                 {
-                    for (int ki = i - 1, kj = 1; ki >= 0 && kj < L; ki--, kj++)
-                        f[i] -= f[ki] * matrix[ki][kj];
+                    for (int ki = i - 1, kj = 1; ki >= 0 && kj < L; ki--, kj++) // верхнетреугольная матрица * у = f
+                        f[i] -= f[ki] * matrix[ki][kj]; // в f будут y 
                     f[i] /= matrix[i][0];
                 }
                 for (int i = N - 1; i >= 0; i--)
                 {
-                    for (int j = i + 1; j < Math.Min(L+i, N); j++)
+                    for (int j = i + 1; j < Math.Min(L+i, N); j++) // нижнетреугольная матрица * x = y
                         f[i] -= f[j] * matrix[i][j - i];
-                    f[i] /= matrix[i][0];
+                    f[i] /= matrix[i][0];  // в f будут x 
                 }
             }
-            using (StreamWriter sw = new StreamWriter("result.txt"))
+            using (StreamWriter sw = new StreamWriter("result.txt")) // можешь тут путь указать
             {
-                if (flag)
+                if (flag) // запись в файл
                 {
                     for (int i = 0; i < N; i++)
                     {
@@ -165,20 +165,20 @@ class Laba2
     while(!solution());
     using(StreamReader sr = new StreamReader("result.txt"))
     {
-        decimal[] x = new decimal[N];
+        decimal[] x = new decimal[N]; // считываем что получилось
         string nums = sr.ReadLine();
         string[] nums_arr = nums.Split(' ');
         for(int i = 0; i < N; i++)
             x[i]= decimal.Parse(nums_arr[i]);
 
-        decimal[] x_a = new decimal[N];
+        decimal[] x_a = new decimal[N]; // считываем что должно быть
         nums = sr.ReadLine();
         nums_arr = nums.Split(' ');
         for(int i = 0; i < N; i++)
             x_a[i]= decimal.Parse(nums_arr[i]);
         sr.Close();
         decimal acc = 0;
-        for(int i = 0; i < N; i++)
+        for(int i = 0; i < N; i++) // считаем погрешность
             acc = Math.Max(acc,Math.Abs(x[i]-x_a[i])/Math.Max(Math.Abs(x_a[i]),(decimal)0.01));
         using(StreamWriter sw = new StreamWriter("acc_test.txt",true))
             {
